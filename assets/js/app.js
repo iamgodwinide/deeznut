786077248afd2c7c89be34e115e31e01ccf64b21
Godwin Idemudia <godwinide@gmail.com>
2023-05-23 21:32:01 +0100

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,6 +21,6 @@ form.addEventListener("submit", e => {
 
 video.addEventListener("ended", ()=> {
     const tweet = `I just registered for $DEEZ nuts @deeznutsethcoin %0A%0A`;
-    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=deeznuts.xyz`, '_blank');
+    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url= deeznutseth.xyz`, '_blank');
     window.location.reload()
 });
